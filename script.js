6aab4372939a2f128a2ac395e3432da07bdcd909
TheProgrammer944 <76252886+TheProgrammer944@users.noreply.github.com>
2025-03-14 20:42:09 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function openBooking(title, image) {
+    document.getElementById("selected-show").textContent = "Booking for: " + title;
+    document.getElementById("show-image").src = image;
+    document.getElementById("booking").style.display = "block";
+}
+
+function calculateTotal() {
+    let adults = parseInt(document.getElementById("adults").value);
+    let kids = parseInt(document.getElementById("kids").value);
+
+    let total = (adults * 50) + (kids * 25);
+    document.getElementById("total-price").textContent = "Total Price: " + total + " AED";
+}
+
+function generateTicket() {
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("time").value;
+    let adults = document.getElementById("adults").value;
+    let kids = document.getElementById("kids").value;
+
+    let ticketDetails = `
+        Show: ${document.getElementById("selected-show").textContent} <br>
+        Date: ${date} <br>
+        Time: ${time} <br>
+        13+ Attendees: ${adults} <br>
+        Under 13 Attendees: ${kids} <br>
+        ${document.getElementById("total-price").textContent}
+    `;
+
+    document.getElementById("ticket-details").innerHTML = ticketDetails;
+    document.getElementById("ticket").style.display = "block";
+}
